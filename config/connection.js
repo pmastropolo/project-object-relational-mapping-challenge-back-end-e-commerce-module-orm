56d778f75bf67fee53d049b00760e00bc1eb036c
amyhytwn@aol.com <135174286+pmastropolo@users.noreply.github.com>
2023-09-12 19:20:47 -0500

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,16 @@
-const Sequelize = require('sequelize');                       // Import Sequelize
-require('dotenv').config();                                   // dotenv
+const Sequelize = require('sequelize'); // Import Sequelize
+require('dotenv').config(); // dotenv
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {                                 // Check if enviroment variable JAWSDB_URL is set
-  sequelize = new Sequelize(process.env.JAWSDB_URL);          // if set,  initialize sequelize with that URL
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-      logging: console.log
-    }
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    logging: console.log
+  }
+);
 
 module.exports = sequelize;
